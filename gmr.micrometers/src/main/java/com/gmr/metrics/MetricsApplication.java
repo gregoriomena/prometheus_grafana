b0c1dc01ea033b0e23a2116ab62d46ea9f3b0681
gregoriomena <gregoriomena@hiberus.com>
2021-10-04 10:54:03 +0200

    1.4. Uso del MeterRegistry - Declaración y ejemplo básico

---
+++ gmr.micrometers/src/main/java/com/gmr/metrics/MetricsApplication.java
@@ -0,0 +1,18 @@
+package com.gmr.metrics;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+
+public class MetricsApplication {
+
+	public static void main(String[] args) {
+		MeterRegistry registry=new SimpleMeterRegistry();
+		
+		Counter counter = registry.counter("numero.empleados", "oficina", "Pepito Perez");
+		counter.increment();
+		counter.increment(200);
+		
+		System.out.printf("Número de empleados %f", counter.count());
+	}
+}
