5e85b9e766ab7785bf71d201392df8097603272d
gregoriomena <gregoriomena@hiberus.com>
2021-10-06 12:06:45 +0200

    5.47. Agregando código para la generación de métricas

--- spring-metrics-push-gateway/src/main/java/com/gmr/metrics/SpringMetricsPushGatewayApplication.java
+++ spring-metrics-push-gateway/src/main/java/com/gmr/metrics/SpringMetricsPushGatewayApplication.java
@@ -5,11 +5,23 @@ import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.Counter;
+import io.prometheus.client.exporter.PushGateway;
+
 @SpringBootApplication
 public class SpringMetricsPushGatewayApplication {
 
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(SpringMetricsPushGatewayApplication.class, args);
+
+		CollectorRegistry registry = new CollectorRegistry();
+		Counter students = Counter.build().name("students_total").help("Total students.").register(registry);
+
+		students.inc(150);
+
+		PushGateway pg = new PushGateway("127.0.0.1:9091");
+		pg.pushAdd(registry, "gmr_students_job");
 	}
 
 }
